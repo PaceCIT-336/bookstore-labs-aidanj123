71c8dee434fb12a47d87c2579d14c7e1943b86ca
aidanj123 <38387090+aidanj123@users.noreply.github.com>
2024-02-17 23:13:37 -0500

    dfadf

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="assets/styles.css">
     <?php session_start();
+    include_once("books.php");
     include_once("cart.php"); ?>
-    include_once("books.php"); ?>
 </head>
 
 <body>
