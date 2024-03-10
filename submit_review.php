4f3d21b6d2b3d419b8980228515b7266ecb7f1f3
aidanj123 <38387090+aidanj123@users.noreply.github.com>
2024-03-09 23:20:21 -0500

    button

--- submit_review.php
+++ submit_review.php
@@ -16,11 +16,16 @@
         }
 
         if (!empty(($_POST['submit']))) {
-            $query = "INSERT INTO reviews(BookID,Rating,Review) values($id,$rating,$review)";
+            $stmt = $pdo->query("INSERT INTO reviews(BookID,Rating,Review) values($id,$rating,$review)");
+            $review = sanitizeString($_POST['review']);
+            if ($review != '') {$review = NULL;}
+            $stmt->execute();
+            if ($stmt->rowCount() == 1) {echo "Review accepted";}
         } else {
             header("Location: review.php"); 
         }
         
         ?>
+        <a href="review.php"><button type="button">Click Me!</button></a>
     </body>
 </html>
